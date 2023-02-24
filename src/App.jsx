5f4b5bf0b18310f79fb5117319762d03967ac263
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-24 13:55:48 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Products from './pages/Products.jsx';
+import NavBar from "./assets/components/navbar/NavBar.jsx";
+
+
 function App() {
 
   return (
     <div>
         <BrowserRouter>
+        <NavBar />
             <Routes>
                 <Route path="/" element={<Home />}/>
                 <Route path="/about" element={<About />}/>
