cd70b91d490cf0f3d6ecd78d1134f08c268f6187
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-26 20:10:06 +0100

    ProductPage

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -2,8 +2,9 @@ import { useParams } from "react-router-dom";
 import Jenson from "../assets/components/products/Jenson";
 import Deon from "../assets/components/products/Deon";
 import Krisha from "../assets/components/products/Krisha";
-import Footer from "../assets/components/footer/Footer";
+import NavBar from "../assets/components/navbar/NavBar";
 import PageNameBorder from "../assets/components/pageNameBorder/PageNameBorder";
+import Footer from "../assets/components/footer/Footer";
 
 const ProductPage = () => {
     
@@ -11,34 +12,30 @@ const ProductPage = () => {
     
     if(params.name === "Jenson"){
         return (
-            <>
-                <PageNameBorder 
-                pageText={"Jenson"}
-                pageTitle={"PRODUCTS"}
-                />
+            <div className="flex flex-col justify-between h-screen">
+                <NavBar />
+                <PageNameBorder pageText={"Jenson"} pageTitle={"PRODUCTS"} />
                 <Jenson />
                 <Footer />
-            </>
+            </div>
         );
     }else if(params.name === "Deon"){
         return (
-            <>
-                <PageNameBorder
-                pageText={"Deon"} 
-                pageTitle={"PRODUCTS"} />
+            <div className="flex flex-col justify-between h-screen">
+                <NavBar />
+                <PageNameBorder pageText={"Deon"} pageTitle={"PRODUCTS"} />
                 <Deon />
                 <Footer />
-            </>
+            </div>
         );
     }else if(params.name === "Krisha"){
         return (
-            <>
-                <PageNameBorder
-                pageText={"Krisha"}
-                pageTitle={"PRODUCTS"} />
+            <div className="flex flex-col justify-between h-screen">
+                <NavBar />
+                <PageNameBorder pageText={"Krisha"} pageTitle={"PRODUCTS"} />
                 <Krisha />
                 <Footer />
-            </>
+            </div>
         );
     }
 }
