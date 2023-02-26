17799865d1e4ef00ab7cb901bd65fcc37e9c905d
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-26 22:00:40 +0100

    background image bug deployment

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 import Footer from "../assets/components/footer/Footer"
 import PageNameBorder from "../assets/components/pageNameBorder/PageNameBorder";
 import NavBar from "../assets/components/navbar/NavBar"
-
+import jensonImg from "../images/Tisch.png"
 /* Product images not shown on deployed page !!!! */
-
+/* Because img not imported on top */
 
 const Products = () => {
     return (
@@ -17,7 +17,7 @@ const Products = () => {
                 />
             </div>
             <main className="flex justify-around flex-wrap h-[25rem] px-36">
-                <div className="pb-6 flex justify-center bg-center w-1/3 bg-no-repeat bg-[url('../../src/images/Tisch.png')]">
+                <div className="pb-6 flex justify-center bg-center w-1/3 bg-no-repeat bg-jensonImg">
                     <h1 className="pt-4 pr-24 font-bold text-lg">Jenson</h1>
                     <Link
                         className="p-2 bg-[#DAA520] mt-auto text-white rounded-md"
@@ -26,7 +26,7 @@ const Products = () => {
                         Shop now
                     </Link>
                 </div>
-                <div className="pb-6 flex justify-center bg-center w-1/3  bg-no-repeat bg-[url('../../src/images/Stuhl.png')]">
+                <div className="pb-6 flex justify-center bg-center w-1/3  bg-no-repeat bg-[url('./Stuhl.png')]">
                     <h1 className="pt-4 pr-24 font-bold text-lg">Deon</h1>
                     <Link
                         className="p-2 bg-[#DAA520] mt-auto text-white rounded-md"
