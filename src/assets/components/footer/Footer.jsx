21bde73e342daa978bd37c580942dba15080c873
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-26 18:28:37 +0100

    fix(footer):footer text centered

--- src/assets/components/footer/Footer.jsx
+++ src/assets/components/footer/Footer.jsx
@@ -4,16 +4,16 @@ import logo from '../../../images/Logo.png';
 const Footer = () => {
     return (
         <footer className="bg-[#F1F1F1] relative">
-            <div className="absolute left-20 top-12 h-[.1rem]  bg-neutral-300 w-2/3 text-[#8D8D8D]" />
+            <div className=" absolute left-20 top-12 h-[.1rem] bg-neutral-300 w-2/3 text-[#8D8D8D]" />
             <section className="flex justify-between h-[25vh] pt-12 pl-20 ">
                 <div className="h-[.1rem] w-1/3 text-[#8D8D8D]">
-                    <img className="pt-2 w-40 mb-3" src={logo} alt="logo" />
+                    <img className="pt-6 w-40 mb-3" src={logo} alt="logo" />
                     <p className="mb-1">(012 8967453)</p>
                     <p className="mb-1">woodies@gmail.com</p>
                     <p className="mb-1">Jakarta, Indonesia</p>
                 </div>
-                <div className="flex justify-center items-end text-[#8D8D8D]">
-                    <p className="w-32 pb-2">© 2020 Woodies</p>
+                <div className="flex justify-center items-end w-full text-[#8D8D8D]">
+                    <p className="w-32 pb-2 text-center">© 2020 Woodies</p>
                 </div>
                 <img src={footerImg} alt="desk" />
             </section>
